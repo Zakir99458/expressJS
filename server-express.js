c13326137378142272ac169bf6cc3cb3ca1900bb
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-05-10 23:46:25 +0300

    server-ui

--- server-express.js
+++ server-express.js
@@ -1,1 +1,13 @@
-const express = require('express')
+const express = require('express');
+
+// Createing an express instance
+const app = express();
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname+"/index.html")
+    console.log(`Request information: ${req}`);
+    console.log(`Response information: ${res}`);
+});
+
+app.listen(5000, () =>{
+    console.log("Server is listening... on port 5000");
+});
